c18e695ca771ddba2edf0ffc5d802fb55dd7a914
Foofy <foozaasksksk@gmail.com>
2016-12-25 01:45:30 +0700

    new updated codec for windows
    fdsagsdgdsg

--- scanner.py
+++ scanner.py
@@ -1,6 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 from re import findall
+import codecs
 
-webpage = open('2559_y1_s2.html', 'r').read()
+file = codecs.open("2559_y1_s2.html", "r", "utf-8")
+webpage = file.read()
 regex = 'faculty_id=\d\d&dept_id=x&curr_id=\d&curr2_id=.&year=\d{4}&semester=\d&class=\d{1,}'
 results = findall(regex, webpage) # a function from re module
 create_text_file = open("results.txt", "w")
