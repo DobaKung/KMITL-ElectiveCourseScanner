cbd52985121caffde624d08a6b8f39a21db9fc15
Foofy <foozaasksksk@gmail.com>
2016-12-25 00:18:05 +0700

    ok create_text_file
    fadsfdsfsdf

--- scanner.py
+++ scanner.py
@@ -3,8 +3,8 @@ from re import findall
 webpage = open('2559_y1_s2.html', 'r').read()
 regex = 'faculty_id=\d\d&dept_id=x&curr_id=\d&curr2_id=.&year=\d{4}&semester=\d&class=\d{1,}'
 results = findall(regex, webpage) # a function from re module
-
+create_text_file = open("results.txt", "w")
 for i in results:
-	print(i)
-
+	create_text_file.write(i)
+create_text_file.close()
 print("Found", len(results), "results.")
