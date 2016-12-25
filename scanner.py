df7d372b4f35bd8d958f8c86bcc3b3af59fb83e6
Nathan Yiangsupapaanontr <zartre.y@gmail.com>
2016-12-25 09:31:47 +0700

    Change file to webpage

--- scanner.py
+++ scanner.py
@@ -1,9 +1,9 @@
 from re import findall
 from codecs import open
 
-file = open("2559_y1_s2.html", "r", "utf-8").read()
+webpage = open("2559_y1_s2.html", "r", "utf-8").read()
 regex = 'faculty_id=\d\d&dept_id=x&curr_id=\d&curr2_id=.&year=\d{4}&semester=\d&class=\d{1,}'
-raw_results = findall(regex, file) # a function from re module
+raw_results = findall(regex, webpage) # a function from re module
 final_results = []
 
 for i in raw_results:
